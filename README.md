70c734083eabc1c0887be4c309dc0e10784c96b9
ngthdan0811 <ngthdan0811@gmail.com>
2024-12-02 22:07:49 +0700

    add swagger

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 - need install : mysql
 - run : npm i --force
 - run : npm start.
+
+- using Railway for deploy.
+- url public : https://capstone-final-dan.up.railway.app/
+- url swagger public : https://capstone-final-dan.up.railway.app/swagger-docs/
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -10,9 +10,47 @@ const routes = express.Router();
  *   post:
  *     summary: User Register
  *     description: Register a new user.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               phone:
+ *                 type: string
+ *               birth_date:
+ *                 type: string
+ *               gender:
+ *                 type: string
+ *                 enum: [male, female]
+ *               type:
+ *                 type: integer
+ *                 enum: [0, 1]
+ *                 description: 0 is guest, 1 is owner.
  *     responses:
  *       200:
  *         description: User registered successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     name:
+ *                       type: string
  *       400:
  *         description: Bad request.
  */
